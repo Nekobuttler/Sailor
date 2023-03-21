142e96475f02b130e24a1dd69625f9de2b9ba393
Nekobuttler <cristiaw86@gmail.com>
2023-03-21 07:48:08 -0600

    Actualizacion del CRUD

---
+++ Views/assets/js/registroClientes.js
@@ -0,0 +1,11 @@
+
+
+
+let contrasenna = $("#contrasenna").val();
+
+let contrasennaConf = $("#contrasenna_confirm").val();
+
+if(contrasenna.trim() != ""  && contrasennaConf.trim() != "" ){
+
+    
+}
